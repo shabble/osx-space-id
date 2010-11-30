5d7c1e4dc1b9e5d5cb60119a8e7a5a02f5b491b7
Duane Johnson <djohnson@getcollc.com>
2010-11-30 16:55:35 -0600

    Added ability to switch spaces (without animation, unfortunately)

--- main.c
+++ main.c
@@ -7,17 +7,29 @@
 #include <Carbon/Carbon.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+#include "CGSPrivate.h"
 
-    int quiet_mode = 0;
+void set_space(int space) {
+  // CFNotificationCenterRef center;
+  // center = CFNotificationCenterGetDistributedCenter();
+  // 
+  // CFNotificationCenterPostNotification(
+  //   center, 
+  //   CFSTR("com.apple.switchSpaces"),
+  //   NULL,
+  //   (const void *)CFSTR("2"),
+  //   TRUE);
+  
+  // NSString *noteName = @"com.apple.switchSpaces";
+  // NSString *spaceID = [NSString stringWithFormat:@"%d", space];
+  // NSNotificationCenter *dCenter = [NSDistributedNotificationCenter defaultCenter];
+  // [dCenter postNotificationName:noteName object:spaceID];
+  
+  CGSConnection conn = _CGSDefaultConnection();
+  CGSSetWorkspace(conn, space);
+}
 
-  /* process the 'quiet' option */
-  if (argc > 1) {
-    if (strncmp(argv[1], "-q", 2) == 0) {
-      quiet_mode = 1;
-    }
-  }
-
+int get_space() {
   CFArrayRef winList = 
     CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
   
@@ -30,13 +42,45 @@ int main(int argc, char* argv[]) {
       const void *thing = CFDictionaryGetValue(winDict, kCGWindowWorkspace);
       CFNumberRef numRef = (CFNumberRef)thing;
       CFNumberGetValue(numRef, kCFNumberIntType, &num);
-        if (quiet_mode == 1) {
-          fprintf(stdout, "%d\n", num);
-        } else {
-          fprintf(stdout, "Current Space ID: %d\n", num);
-        }
-      break;
+      return num;
     }
   }
-  return num;
+}
+
+int main(int argc, char* argv[]) {
+  
+  int quiet_mode = 0;
+  int space = 0;
+  
+  if (argc > 1) {
+    int argi = 1;
+    while (argi < argc) {
+      /* process the 'quiet' option */
+      if (strncmp(argv[argi], "-q", 2) == 0) {
+        quiet_mode = 1;
+      }
+      
+      /* process the 'set space' option */
+      if (strncmp(argv[argi], "-s", 2) == 0) {
+        space = atoi(argv[++argi]);
+        if (space == 0) {
+          fprintf(stdout, "Could not convert space to integer: %s\n", argv[2]);
+        }
+      }
+      argi++;
+    }
+  }
+  
+  if (space == 0) { /* get space */
+    space = get_space();
+    if (quiet_mode == 1) {
+      fprintf(stdout, "%d\n", space);
+    } else {
+      fprintf(stdout, "Current Space ID: %d\n", space);
+    }
+  } else { /* set space */
+    set_space(space);
+    if (quiet_mode == 0)
+      fprintf(stdout, "Switched to %d\n", space);
+  }
 }
