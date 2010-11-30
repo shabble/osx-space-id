9a409a0b3743c8618c04775f32f64274a7b07408
Duane Johnson <djohnson@getcollc.com>
2010-11-30 15:40:09 -0600

    Convert tabs to spaces

--- main.c
+++ main.c
@@ -18,26 +18,25 @@ int main(int argc, char* argv[]) {
         }
     }
 
- 	CFArrayRef winList = 
-	    CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
-	int len = CFArrayGetCount(winList);
-	int i, num = 0;
-	for (i = 0; i < len; i++) {
-		CFDictionaryRef winDict = CFArrayGetValueAtIndex(winList, i);
-		if (CFDictionaryContainsKey(winDict, kCGWindowWorkspace)) {
-			//fprintf(stderr, "Yay, contains that key for i: %d\n", i);
-			const void *thing = CFDictionaryGetValue(winDict, kCGWindowWorkspace);
-			CFNumberRef numRef = (CFNumberRef)thing;
-			CFNumberGetValue(numRef, kCFNumberIntType, &num);
-
-            if (quiet_mode == 1) {
-                fprintf(stdout, "%d\n", num);
-            } else {
-                fprintf(stdout, "Current Space ID: %d\n", num);
-            }
-
-			break;
-		}
-	}
-	return num;
+  CFArrayRef winList = 
+    CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
+  
+  int len = CFArrayGetCount(winList);
+  int i, num = 0;
+  for (i = 0; i < len; i++) {
+    CFDictionaryRef winDict = CFArrayGetValueAtIndex(winList, i);
+    if (CFDictionaryContainsKey(winDict, kCGWindowWorkspace)) {
+      //fprintf(stderr, "Yay, contains that key for i: %d\n", i);
+      const void *thing = CFDictionaryGetValue(winDict, kCGWindowWorkspace);
+      CFNumberRef numRef = (CFNumberRef)thing;
+      CFNumberGetValue(numRef, kCFNumberIntType, &num);
+        if (quiet_mode == 1) {
+          fprintf(stdout, "%d\n", num);
+        } else {
+          fprintf(stdout, "Current Space ID: %d\n", num);
+        }
+      break;
+    }
+  }
+  return num;
 }
