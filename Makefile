92daf2df630e89d510736f47bfd4d6a1b48ddef1
Tom Feist <shabble@metavore.org>
2011-06-20 13:36:28 +0100

    modified to allow animated switching between spaces with -a -s <num>, also a few other features. See the source or ./spacefinder -h for details.

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
+CFLAGS = -x objective-c -arch i386 -fmessage-length=0 -std=c99 \
+		 -fpascal-strings -fasm-blocks \
+		-isysroot /Developer/SDKs/MacOSX10.5.sdk \
+		-mmacosx-version-min=10.5 -framework Foundation \
+		-framework Carbon
+
 .PHONY: clean
 
 spacefinder: main.c
-	gcc -o spacefinder $< -framework Carbon
+	gcc $(CFLAGS) -o spacefinder $< 
 clean:
 	-rm spacefinder
--- main.c
+++ main.c
@@ -1,32 +1,46 @@
-//
-//  main.c
-//  spacefinder
-//
-//  Created by shabble on 04/03/2010.
-//  
-#include <Carbon/Carbon.h>
-#include <stdio.h>
+/*
+ * main.c
+ * spacefinder
+ *
+ * + Created by shabble on 04/03/2010.
+ * + Added animated transition, and refactored to
+ *    use getopt, 20/6/2011.
+ *
+ */
 
+#import <Carbon/Carbon.h>
+#import <Foundation/Foundation.h>
+
+#include <stdio.h>
+#include <unistd.h>
 #include "CGSPrivate.h"
 
-void set_space(int space) {
-    // CFNotificationCenterRef center;
-    // center = CFNotificationCenterGetDistributedCenter();
-    // 
-    // CFNotificationCenterPostNotification(
-    //   center, 
-    //   CFSTR("com.apple.switchSpaces"),
-    //   NULL,
-    //   (const void *)CFSTR("2"),
-    //   TRUE);
+/* definitions for a couple of useful functions, from
+ * http://meeu.me/blog/dashboard-expose-spaces
+ */
+ 
+void CoreDockSendNotification(NSString *notificationName);
+BOOL CoreDockGetWorkspacesEnabled();
+void CoreDockGetWorkspacesCount(int *rows, int *columns);
+
+void set_space(int space, BOOL animate) {
   
-    // NSString *noteName = @"com.apple.switchSpaces";
-    // NSString *spaceID = [NSString stringWithFormat:@"%d", space];
-    // NSNotificationCenter *dCenter = [NSDistributedNotificationCenter defaultCenter];
-    // [dCenter postNotificationName:noteName object:spaceID];
-  
-    CGSConnection conn = _CGSDefaultConnection();
-    CGSSetWorkspace(conn, space);
+    if (animate) {
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        /* code for animated switching found here:
+           http://meeu.me/blog/dashboard-expose-spaces
+        */
+        NSString *noteName = @"com.apple.switchSpaces";
+        NSString *spaceID = [NSString stringWithFormat:@"%d", (int)space-1];
+        NSNotificationCenter *dCenter = [NSDistributedNotificationCenter defaultCenter];
+        [dCenter postNotificationName:noteName object:spaceID];
+        
+        [pool drain];
+    } else {
+        CGSConnection conn = _CGSDefaultConnection();
+        CGSSetWorkspace(conn, space);
+    }
+
 }
 
 int get_space() {
@@ -47,43 +61,139 @@ int get_space() {
     }
 }
 
+void show_help_and_exit() {
+    fprintf(stderr, "%s\n",
+            "spacefinder: [-a -q -r -s <num>]\n"
+            " -a animates space transition (only when -s is used)\n"
+            " -q quiet mode, prints a single number with the current space ID\n"
+            " -qq prints no output at all. Only useful with -r or -s\n"
+            " -r encodes the space ID in the process return value\n"
+            " -n returns the dimensions of the Spaces configuration\n"
+            " -s <num> sets the current space to <num>. Reading args are not used\n"
+            "  in conjunction with -s"
+
+        );
+    exit(0);
+}
+
 int main(int argc, char* argv[]) {
   
-    int quiet_mode = 0;
-    int space = 0;
-  
-    if (argc > 1) {
-        int argi = 1;
-        while (argi < argc) {
-            /* process the 'quiet' option */
-            if (strncmp(argv[argi], "-q", 2) == 0) {
-                quiet_mode = 1;
+    BOOL quiet_mode         = NO;
+    BOOL silent_mode        = NO;
+    BOOL animate_transition = NO;
+    BOOL use_return_value   = NO;
+    BOOL should_set_space   = NO;
+    BOOL display_dimensions = NO;
+
+    int c;
+
+    int rows = 0;
+    int cols = 0;
+    int num_spaces = 0;
+    long space     = -1;
+
+    if (!CoreDockGetWorkspacesEnabled()) {
+        fprintf(stdout, "Spaces is not enabled\n");
+        return -1;
+    }
+
+    CoreDockGetWorkspacesCount(&rows, &cols);
+    num_spaces = rows * cols;
+
+    while ((c = getopt(argc, argv, "hnrqas:")) != -1) {
+        switch(c) {
+        case 'h':
+            show_help_and_exit();
+            break;
+            
+        case 'n':
+            display_dimensions = YES;
+            break;
+        case 'r':
+            use_return_value = YES;
+            break;
+        case 'q':
+            quiet_mode = YES;
+            
+            /* seen for the 2nd time */
+            if (quiet_mode == YES) {
+                silent_mode = YES;
             }
-      
-            /* process the 'set space' option */
-            if (strncmp(argv[argi], "-s", 2) == 0) {
-                space = atoi(argv[++argi]);
-                if (space == 0) {
-                    fprintf(stdout, 
-                            "Could not convert space to integer: %s\n",
-                            argv[2]);
-                }
+            break;
+        case 'a':
+            animate_transition = YES;
+            break;
+        case 's':
+            if (strlen(optarg)) {
+                should_set_space = YES;
+                space = strtol(optarg, NULL, 10);
+            } else {
+                fprintf (stderr,
+                         "Space number %s could not be parsed into a number",
+                         optarg);
+                return -1;
             }
-            argi++;
+            break;
+        case '?':
+            if (optopt == 's') {
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            } else if (isprint (optopt)) {
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            } else {
+                fprintf (stderr,
+                         "Unknown option character `\\x%x'.\n",
+                         optopt);
+            }
+            return -1;
         }
     }
   
-    if (space == 0) { /* get space */
+    if (space > 0 && should_set_space) { /* set space */
+        if (space > num_spaces) {
+            fprintf(stderr, "Cannot switch beyond maximum number of spaces\n");
+            return -1;
+        }
+
+        set_space(space, animate_transition);
+
+        if (!quiet_mode) {
+            fprintf(stdout, "Switched to %d\n", space);
+        }
+        
+        return 0;
+
+    } else { /* get space */
+
         space = get_space();
-        if (quiet_mode == 1) {
-            fprintf(stdout, "%d\n", space);
+
+        if (quiet_mode) {
+            if (!silent_mode) {
+                fprintf(stdout, "%d\n", space);
+            }
         } else {
             fprintf(stdout, "Current Space ID: %d\n", space);
         }
-    } else { /* set space */
-        set_space(space);
-        if (quiet_mode == 0)
-            fprintf(stdout, "Switched to %d\n", space);
-    }
 
+        if (display_dimensions) {
+            if (rows > 0 && cols > 0) {
+                if (quiet_mode) {
+                    if (!silent_mode) {
+                        fprintf(stdout, "%dx%d\n", rows, cols);
+                    }
+                } else {
+                    fprintf(stdout, "Spaces dimensions: %d rows, %d columns\n",
+                            rows, cols);
+                }
+            } else {
+                fprintf(stderr, "Could not determine the Spaces dimensions\n");
+                return -1;
+            }
+        }
+
+        if (use_return_value) {
+            return space;
+        } else {
+            return 0;
+        }
+    }
 }
